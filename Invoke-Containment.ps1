5696e1c4acdfd63104a246fd7a3646b95eaf8c73
21gramaz <fernando.fujikawa@gmail.com>
2021-03-28 21:36:16 +1300

    commit

--- Invoke-Containment.ps1
+++ Invoke-Containment.ps1
@@ -1,52 +1,214 @@
 <#Invoke-Contaiment
 This module will have all functions to 
 
-function addfirewallrule
-{
-    write-host "adding local windows firewall rule 'Block Outbound connections"
-    Invoke-Command -Session $session -ScriptBlock {New-NetFirewallRule -DisplayName "Block Outbound connections" -Direction Outbound -Action Block}
-    write-host "host isolated. (it will still accept inbound connections)"
-}
-
-function removefirewallrule
-{
-    write-host "removing local windows firewall rule 'Block Outbound connections"
-    Invoke-Command -Session $session -ScriptBlock {Remove-NetFirewallRule -DisplayName "Block Outbound connections"}
-    write-host "host released from isolation"
-}
 #>
-
-
+$ErrorActionPreference = "stop"
 function Invoke-NetworkIsolation {
     param (
         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [string[]]    
-        $ComputerName
+        $ComputerName,
+
+        [Parameter()]
+        [switch]
+        $Localhost,
+
+        [Parameter()]
+        [System.Management.Automation.Runspaces.PSSession]
+        $Session
     )
-    write-host "adding local windows firewall rule 'Block Outbound connections'"
-    $firewallisolationrule={New-NetFirewallRule -DisplayName "Block Outbound connections" -Direction Outbound -Action Block;
-    New-NetFirewallRule -DisplayName "Block Inbound connections" -Direction Inbound -Action Block -LocalPort 0-5984,5987-65535 -Protocol TCP
-        $firewallprofiles=Get-NetFirewallProfile;
-        foreach ($profile in $firewallprofiles){
-            Set-NetFirewallProfile -Name ($profile).Name -Enabled True
+    begin
+    {
+        write-host "[*]["$Session.ComputerName"] Attempting to add local windows firewall rule 'Block Outbound connections'. Nothing out is allowed" -ForegroundColor Yellow
+        write-host "[*]["$Session.ComputerName"] Attempting to add local windows firewall rule 'Block Inbound connections'. Just RDP and WinRM are allowed" -ForegroundColor Yellow 
+        
+        #Commands that will be executed to add the firewall rules the server
+        #It will block all incoming traffic this means all servers will stop to work like IIS/Exchange Server/AD.
+        $firewallisolationrule={
+            #New-NetFirewallRule -DisplayName "Block Outbound connections" -Direction Outbound -Action Block -RemotePort 0-87,89-388,390-65535 -Protocol UDP;
+            #New-NetFirewallRule -DisplayName "Block Outbound connections" -Direction Outbound -Action Block -Protocol TCP;
+            New-NetFirewallRule -DisplayName "Block Outbound connections" -Direction Outbound -Action Block -LocalPort 0-87,89-65535 -Protocol UDP
+            New-NetFirewallRule -DisplayName "Block Inbound connections" -Direction Inbound -Action Block -LocalPort 0-5984,5987-65535 -Protocol TCP
+            $firewallprofiles=Get-NetFirewallProfile;
+            foreach ($profile in $firewallprofiles)
+            {
+                Set-NetFirewallProfile -Name ($profile).Name -Enabled True
+            }
+        }
+        $parameters=@{scriptblock = $firewallisolationrule}
+
+        #Commands that will be executed to check if the rules had been added.
+        $containmentstatus={
+            $CheckingRulesAdded=Get-NetFirewallRule | Where-Object {$_.DisplayName -match '(Block Outbound connections)|(Block Inbound connections)'}
+            if($CheckingRulesAdded.Count -eq 2)
+            {
+                write-host "[+] Firewall rules successfully added" -ForegroundColor Green
+                Write-Host "[+] Rules identifiers: " $CheckingRulesAdded.Name -ForegroundColor Green
+                Write-Host "[*] Warning: If you are running this script to a remote computer do not close this Powershell Console you might lose the last working session" -ForegroundColor Yellow
+            }
+            else{
+                write-host "[-] Firewall rules could not be found, something went wrong" -ForegroundColor Red
+                exit
+            }   
+        }
+        $statusparameters=@{scriptblock = $containmentstatus}
+
+    }
+    #Applying FW rules
+    Process
+    {
+        if($Localhost)
+        {
+            try{
+                Invoke-Command @parameters -ErrorAction Stop | Out-Null
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit           
+            }
+        }
+        else
+        {
+            try{
+                Invoke-Command -Session $Session @parameters -ErrorAction Stop | Out-Null
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit
+            }
+        }
+    }
+    
+    #Checking if the firewall rules had been applied.
+    end
+    {
+        if($Localhost)
+        {
+            try{
+                Invoke-Command @statusparameters -ErrorAction Stop | Out-Null
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit        
+            }
+        }
+        else 
+        {
+            try{
+                Invoke-Command -Session $Session @statusparameters -ErrorAction Stop | Out-Null
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit
+            }
         }
     }
 
-    $parameters=@{scriptblock = $firewallisolationrule}
-
-    Invoke-Command -ComputerName $ComputerName @parameters
+    
 }
 function Invoke-NetworkRelease {
     param (
         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [string[]]    
-        $ComputerName
+        $ComputerName,
+
+        [Parameter()]
+        [switch]
+        $LocalHost,
+
+        [Parameter()]
+        [System.Management.Automation.Runspaces.PSSession]
+        $Session
     )
-    $firewallisolationrule={Remove-NetFirewallRule -DisplayName "Block Outbound connections"
-    Remove-NetFirewallRule -DisplayName "Block Inbound connections"}
-    $parameters=@{scriptblock = $firewallisolationrule}
-    write-host "removing local windows firewall rule 'Block Outbound connections"
-    Invoke-Command -ComputerName $ComputerName @parameters
+
+    begin
+    {
+        
+        write-host "[*]["$Session.ComputerName"] Removing local windows firewall rule 'Block Outbound connections'" -ForegroundColor Yellow
+        write-host "[*]["$Session.ComputerName"] Removing to remove local windows firewall rule 'Block Inbound connections'" -ForegroundColor Yellow
+        #commands to be performed to release from isolation.
+        $firewallremoverules={
+            Remove-NetFirewallRule -DisplayName "Block Outbound connections"
+            Remove-NetFirewallRule -DisplayName "Block Inbound connections"
+        }
+        $parameters=@{scriptblock = $firewallremoverules}
+
+        #Commands to check of the rules had been removed.
+        $containmentstatus={
+            $CheckingRulesAdded=Get-NetFirewallRule | Where-Object {$_.DisplayName -match '(Block Outbound connections)|(Block Inbound connections)'}
+            if($CheckingRulesAdded.Count -eq 2)
+            {
+                write-host "[-] Firewall could not be removed, please check your permissions and connectivity" -ForegroundColor Red
+                Write-Host "Rules identifiers: " $CheckingRulesAdded.Name -ForegroundColor Red
+                exit
+            }
+            else{
+                write-host "[-] Firewall rules successfully removed " -ForegroundColor Green
+            }   
+        }
+        $statusparameters=@{scriptblock = $containmentstatus}
+
+    }
+
+    process
+    {
+        if($LocalHost)
+        {
+            try
+            {
+                Invoke-Command @parameters -ErrorAction Stop
+            }
+            catch
+            {
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+            }
+        }
+        else
+        {
+            try
+            {
+                Invoke-Command -Session $Session @parameters -ErrorAction Stop
+            }
+            catch
+            {
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+            }
+        }
+    }
+    
+    end
+    {
+        if($Localhost)
+        {
+            try{
+                Invoke-Command @statusparameters -ErrorAction Stop 
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit        
+            }
+        }
+        else 
+        {
+            try{
+                Invoke-Command -Session $Session @statusparameters -ErrorAction Stop | Out-Null
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+                exit
+            }
+        }
+    }
+
+
 }
 function Invoke-Containment{
     param(
@@ -56,19 +218,83 @@ function Invoke-Containment{
 
         [Parameter()]
         [Object]
-        $ComputerInfo
+        $ComputerInfo,
+
+        [Parameter()]
+        [switch]
+        $LocalHost,
+
+        [Parameter()]
+        [System.Management.Automation.Runspaces.PSSession]
+        $Session
     )
 
-
-    if($ContainmentType -eq "NetworkIsolation"){
-        Invoke-NetworkIsolation -ComputerName $ComputerInfo.ComputerName
+    if($LocalHost)
+    {
+        if($ContainmentType -eq "NetworkIsolation")
+        { 
+            Invoke-NetworkIsolation -LocalHost
+        }
+        elseif($ContainmentType -eq "NetworkRelease")
+        { 
+            Invoke-NetworkRelease -LocalHost
+        }
+        else 
+        {
+            write-host "[-] Containment type not existent or not implemented, check spelling and try again." -ForegroundColor Red
+        }
     }
-    if($ContainmentType -eq "NetworkRelease"){
-        Invoke-NetworkRelease -ComputerName $ComputerInfo.ComputerName 
+    else
+    {
+        if($ContainmentType -eq "NetworkIsolation")
+        { 
+            Invoke-NetworkIsolation -ComputerName $ComputerInfo.ComputerName -Session $Session
+        }
+        elseif($ContainmentType -eq "NetworkRelease")
+        { 
+            Invoke-NetworkRelease -ComputerName $ComputerInfo.ComputerName -Session $Session 
+        }
+        else 
+        {
+            write-host "[-] Containment type not existent or not implemented, check spelling and try again." -ForegroundColor Red
+        }
     }
-
 }
 
+function Invoke-ContainmentConsole
+{
+    param(
+        [Parameter()]
+        [Object]
+        $ComputerInfo,
+
+        [Parameter()]
+        [System.Management.Automation.Runspaces.PSSession]
+        $Session
+    )
+    do
+    {
+        try
+        {
+            Write-Host "ContaimentConsole> " -NoNewLine
+            $command = $Host.UI.ReadLine()
+            Invoke-Command -Session $Session -ScriptBlock {invoke-expression $using:command}
+            #Invoke-Command -ScriptBlock {invoke-expression $using:command}
+        }
+        catch
+        {
+            write-host "[-] Houston we had a problem... " -ForegroundColor Red
+            Write-Host $_ -ForegroundColor Red
+        }
+    }while($command -ne "exit")
+    #Checks if the remote session is in an available state
+    <#$remotesessions=Get-PSSession
+    foreach ($remotesession in $remotesessions)
+    {
+        if ($remotesession -eq ){}
+    }#>
+    Invoke-NetworkRelease  -ComputerName $ComputerInfo.ComputerName -Session $Session 
+}
 
 
 #Export-ModuleMember function Get-Containment
--- Invoke-SecurityIncidentResponse.ps1
+++ Invoke-SecurityIncidentResponse.ps1
@@ -9,27 +9,28 @@ param (
     [string[]]
     $ComputerName,
 
-    [Parameter(Mandatory=$true,ParameterSetName = "Collection")]
+    [Parameter(ParameterSetName = "Collection")]
     [switch]
     $Collection,
 
-    [Parameter(Mandatory=$true,ParameterSetName = "Contaiment")]
+    [Parameter(ParameterSetName = "Contaiment")]
     [switch]
     $Containment,
 
-    [Parameter(Mandatory=$true,ParameterSetName = "Remediation")]
+    [Parameter(ParameterSetName = "Remediation")]
     [switch]
     $Remediation,
 
-    [Parameter(ParameterSetName = "Collection")]
+    [Parameter(Mandatory=$true,ParameterSetName = "Collection")]
     [string]
     $CollectionType,
     
-    [Parameter(ParameterSetName = "Contaiment")]
+    [Parameter(Mandatory=$true,ParameterSetName = "Contaiment")]
+    [ValidateSet("NetworkIsolation", "NetworkRelease")]
     [string]
     $ContainmentType,
 
-    [Parameter(ParameterSetName = "Remediation")]
+    [Parameter(Mandatory=$true,ParameterSetName = "Remediation")]
     [switch]
     $RemediationType,
 
@@ -38,26 +39,43 @@ param (
 )
 
 begin{
+    $ErrorActionPreference = "stop"
     function get-basicinfo {
         param(
             [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
             [string[]]    
             $ComputerName,
             
+            [Parameter()]
             [System.Management.Automation.PSCredential]
-            $Credential
+            $Credential,
+
+            [Parameter()]
+            [System.Management.Automation.Runspaces.PSSession]
+            $Session
         )
 
-            $command={$hostinfo=Get-CimInstance Win32_OperatingSystem;
+            $command={
+                $hostinfo=Get-CimInstance Win32_OperatingSystem;
                 $hostinfo | Add-Member -NotePropertyName PowershellVersion -NotePropertyValue $PSVersionTable; 
-                $hostinfo}
-            $parameters=@{scriptblock = $command}
-            if ($Credential) {
-                $parameters.Credential = $Credential
+                $hostinfo
             }
+            $parameters=@{scriptblock = $command}
+            #if ($Credential) {
+                #$parameters.Credential = $Credential
+            #}
                 if($null -ne $ComputerName){
-                    $hostinfo=Invoke-Command -ComputerName $ComputerName @parameters
-                    $hostinfo | Add-Member -NotePropertyName ComputerName -NotePropertyValue $ComputerName
+                    write-host "[*] Gathering basic information about the host" -ForegroundColor Yellow
+                    try{ 
+                        $hostinfo=Invoke-Command -Session $Session @parameters -ErrorAction Stop 
+                        $hostinfo | Add-Member -NotePropertyName ComputerName -NotePropertyValue $ComputerName -ErrorAction Stop
+                    }
+                    catch {
+                        write-host "[-] Houston we had a problem gathering the basic info... " -ForegroundColor Red
+                        Write-Host $_ -ForegroundColor Red
+                        exit
+                    }
+                    
                     return $hostinfo
                 }
                 else{
@@ -67,22 +85,102 @@ begin{
     }
 }
 process{
+    #Collecting credentials if necessary
     if($usecreds){
         $creds=Get-Credential
     }
+
+    #Initiating session if not local
+    #Needs to be done on multiple sessions:
+    if($null -ne $ComputerName)
+    {
+        $sessions=@()
+        $counter=0
+        try {
+            if($usecreds)
+            {
+                $sessions=New-PSSession $ComputerName -Credential $creds -Name $ComputerName -ErrorAction Stop
+            }
+            else
+            {
+                foreach($computer in $ComputerName)
+                {
+                    $sessionName="Containment"+$counter
+                    $sessions+=New-PSSession $computer -Name $sessionName -ErrorAction Stop
+                    $counter++
+                }
+            }
+        }
+        catch 
+        {
+            Write-host "Houston we had a problem..." -ForegroundColor Red
+            Write-host $_ -ForegroundColor Red
+            exit 
+        }
+    }
+
+    #Getting basic info
+    $hostsinfo=@()
+    try
+    {
+        foreach ($session in $sessions)
+        {
+            $hostsinfo+=get-basicinfo -ComputerName $ComputerName -Session $session -ErrorAction Stop
+        }
+    }
+    catch{
+        write-host "[-] Houston we had a problem... " -ForegroundColor Red
+        Write-Host $_ -ForegroundColor Red
+        exit
+    }
     
-    $hostsinfo=get-basicinfo -ComputerName $ComputerName -Credential $creds
-    foreach ($hostinfo in $hostsinfo){
+    foreach ($hostinfo in $hostsinfo)
+    {
+        write-host "[+] Basic information gathered Host: " $hostinfo.CSName ", OS Version: " $hostinfo.Version ", Arch: " $hostinfo.OSArchitecture " PS version: "  $hostinfo.PowershellVersion.PSVersion -ForegroundColor Green
+    }
+    
+    #Call IR phase
+    if($null -ne $ComputerName)
+    {
+        foreach ($session in $sessions)
+        {
+            if ($Collection){
+                write-host "Startin Collection"
+            }
+            if ($Containment){
+                write-host "[+] Starting Containment" -ForegroundColor Green
+                try{
+                    Remove-Module -Name Invoke-Containment -ErrorAction SilentlyContinue
+                    Import-Module -Name "$PSScriptRoot\Invoke-Containment.ps1"
+                    Invoke-Containment -ContainmentType $ContainmentType -ComputerInfo $hostinfo -Session $session
+                    #Invoke-ContainmentConsole -Session $session -ComputerInfo $hostsinfo
+                }
+                catch{
+                    write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                    Write-Host $_ -ForegroundColor Red
+                }
+            }
+            if ($Remediation){
+                write-host "Startin Remediation"
+            }
+        }
+    }
+    else{
         if ($Collection){
             write-host "Startin Collection"
         }
         if ($Containment){
-            write-host "Startin Containment"
-            
-            Import-Module -Name "$PSScriptRoot\Invoke-Containment.ps1"
-            
-            Invoke-Containment -ContainmentType $ContainmentType -ComputerInfo $hostinfo 
-            
+            write-host "[+] Starting Containment" -ForegroundColor Green
+            try{
+                Remove-Module -Name Invoke-Containment -ErrorAction SilentlyContinue
+                Import-Module -Name "$PSScriptRoot\Invoke-Containment.ps1" 
+                if($ComputerName){
+                Invoke-Containment -ContainmentType $ContainmentType -ComputerInfo $hostinfo -LocalHost}
+            }
+            catch{
+                write-host "[-] Houston we had a problem... " -ForegroundColor Red
+                Write-Host $_ -ForegroundColor Red
+            }
         }
         if ($Remediation){
             write-host "Startin Remediation"
